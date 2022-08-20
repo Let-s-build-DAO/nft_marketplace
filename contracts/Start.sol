b2362e634b647fee30b39d1f742e0e6eceb9bee6
Remix Workspace <>
2022-08-20 17:17:17 +0100

    simple vars in sol

---
+++ contracts/Start.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Start { 
+
+    // To make your vars visible add a visibility keyword
+    
+    string public bob = 'Bob';
+    bool public isBobMale = true;
+    uint public ageOfBob = 2;
+    int public Dob = 2020;
+    address public owner = msg.sender;
+
+    
+
+}
